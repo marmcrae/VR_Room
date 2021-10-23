72fe997161b69e0e44783c9215d264cb3d010d84
Mar McRae <56138234+marmcrae@users.noreply.github.com>
2021-10-22 22:25:24 -1000

    Adds sockets for interactables

---
+++ VR Room Project/Assets/ResetPosition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+
+    private Vector3 _resetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.Get)
+    }
+}
